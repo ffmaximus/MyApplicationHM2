89a473d7674c372de4a7527daa22adac75cbe6b0
asus pc <ffamaximus@yahoo.com>
2017-04-26 03:55:49 -0500

    Initial 2

---
+++ app/src/main/java/unipanamericana/app/hm/com/myapplicationhm/MainActivity.java
@@ -0,0 +1,13 @@
+package unipanamericana.app.hm.com.myapplicationhm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
